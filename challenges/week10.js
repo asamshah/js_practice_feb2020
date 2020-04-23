2287ac4584ed768e4e936f53f867382d8ad4c484
Asam Shah <asamshah@gmail.com>
2020-04-23 18:29:39 +0100

    Week10 Challenges Completed

---
+++ challenges/week10.js
@@ -0,0 +1,177 @@
+const Sugar = require("sugar");
+Sugar.extend();
+/**
+ * This function takes a number, e.g. 123 and returns the sum of all its digits, e.g 6 in this example.
+ * @param {Number} n
+ */
+const sumDigits = (n) => {
+  if (n === undefined) throw new Error("n is required");
+  if (!Number(n)) throw new Error("number is required");
+  let arr = n.toString().split("");
+  return arr.map((n) => Number(n)).reduce((acc, curr) => acc + curr);
+};
+
+/**
+ * This function creates a range of numbers as an array. It received a start, an end and a step. Step is the gap between numbers in the range. For example, if start = 3, end = 11 and step = 2 the resulting range would be: [3, 5, 7, 9, 11]
+ * Both the start and the end numbers are inclusive.
+ * Step is an optional parameter. If it is not provided, assume the step is 1.
+ * @param {Number} start
+ * @param {Number} end
+ * @param {Number} step
+ */
+const createRange = (start, end, step) => {
+  if (start === undefined) throw new Error("start is required");
+  if (end === undefined) throw new Error("end is required");
+  if (step === undefined || step < 0) {
+    step = 1;
+  }
+  if (start > end) {
+    start = [end, (end = start)][0]; //value exchange between variables
+  }
+  let arr = [];
+  for (let i = start; i <= end; i += step) {
+    arr.push(i);
+  }
+  if (arr[arr.length - 1] < end) {
+    arr.push(end);
+  }
+
+  return arr;
+};
+
+/**
+ * This function takes an array of user objects and their usage in minutes of various applications. The format of the data should be as follows:
+ * [
+ *  {
+ *    username: "beth_1234",
+ *    name: "Beth Smith",
+ *    screenTime: [
+ *                 { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40} },
+ *                 { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31} },
+ *                 { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19} },
+ *                 { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61} },
+ *                ]
+ *   },
+ *   {
+ *    username: "sam_j_1989",
+ *    name: "Sam Jones",
+ *    screenTime: [
+ *                 { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10} },
+ *                 { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16} },
+ *                 { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31} },
+ *                ]
+ *   },
+ * ]
+ *
+ * The function should return an array of usernames of users who have used more than 100 minutes of screentime for a given date.
+ * The date will be provided in the format "2019-05-04" (YYYY-MM-DD)
+ * For example, if passed the above users and the date "2019-05-04" the function should return ["beth_1234"] as she used over 100 minutes of screentime on that date.
+ * @param {Array} users
+ */
+const getScreentimeAlertList = (users, date) => {
+  if (users === undefined) throw new Error("users is required");
+  if (date === undefined) throw new Error("date is required");
+  if (!Array.isArray(users)) throw new Error("Incorrect type of data");
+  if (typeof date !== "string") throw new Error("In");
+
+  let result = [];
+
+  users.map(
+    (obj) =>
+      obj.screenTime
+        .filter((d) => d.date === date)
+        .map((u) => Object.values(u.usage).reduce((a, b) => a + b)) > 100 &&
+      result.push(obj.username)
+  );
+
+  return result;
+};
+
+/**
+ * This function will receive a hexadecimal color code in the format #FF1133. A hexadecimal code is a number written in hexadecimal notation, i.e. base 16. If you want to know more about hexadecimal notation:
+ * https://www.youtube.com/watch?v=u_atXp-NF6w
+ * For colour codes, the first 2 chars (FF in this case) represent the amount of red, the next 2 chars (11) represent the amound of green, and the last 2 chars (33) represent the amount of blue.
+ * Colours can also be represented in RGB format, using decimal notation.
+ * This function should transform the hex code into an RGB code in the format:
+ * "rgb(255,17,51)"
+ * Hint: You will need to convert each hexadecimal value for R, G and B into its decimal equivalent!
+ * @param {String} str
+ */
+const hexToRGB = (hexStr) => {
+  if (hexStr === undefined) throw new Error("hexStr is required");
+  if (hexStr[0] !== "#") throw new Error("The string must start with #");
+  let obj = {
+    "0": 0,
+    A: 10,
+    B: 11,
+    C: 12,
+    D: 13,
+    E: 14,
+    F: 15,
+  };
+
+  let newArr = hexStr.toUpperCase().slice(1).split("");
+
+  let arr = [];
+
+  newArr.forEach((w) =>
+    Number(w)
+      ? arr.push(Number(w))
+      : Object.keys(obj).filter((c) => w === c && arr.push(obj[c]))
+  );
+
+  let twoDimArr = arr
+    .map((e, i) => i % 2 === 0 && arr.slice(i, i + 2))
+    .filter((e) => e);
+
+  let r =
+    twoDimArr[0] === undefined || twoDimArr[0].length < 2
+      ? "Incorrect"
+      : twoDimArr[0].reduce((a, b) => a * 16 + b);
+  let g =
+    twoDimArr[1] === undefined || twoDimArr[1].length < 2
+      ? "Incorrect"
+      : twoDimArr[1].reduce((a, b) => a * 16 + b);
+  let b =
+    twoDimArr[2] === undefined || twoDimArr[2].length < 2
+      ? "Incorrect"
+      : twoDimArr[2].reduce((a, b) => a * 16 + b);
+
+  return r === "Incorrect" || g === "Incorrect" || b === "Incorrect"
+    ? "This error is becouse you entered an incorrect character or the number of characters are incorrect, only 'A','B','C','D','E','F' are alowed"
+    : "rgb(" + `${r},${g},${b}` + ")";
+};
+
+/**
+ * This function takes a noughts and crosses board represented as an array, where an empty space is represented with null.
+ * [
+ *  ["X", "0", null],
+ *  ["X", null, "0"],
+ *  ["X", null, "0"]
+ * ]
+ * The function should return "X" if player X has won, "0" if the player 0 has won, and null if there is currently no winner.
+ * @param {Array} board
+ */
+const findWinner = (board) => {
+  if (board === undefined) throw new Error("board is required");
+  let arr = [].concat(...board);
+  let result = "";
+  if (arr[0] === arr[1] && arr[0] === arr[2]) result = arr[0];
+  else if (arr[3] === arr[4] && arr[3] === arr[5]) result = arr[3];
+  else if (arr[6] === arr[7] && arr[6] === arr[8]) result = arr[6];
+  else if (arr[0] === arr[3] && arr[0] === arr[6]) result = arr[0];
+  else if (arr[1] === arr[4] && arr[1] === arr[7]) result = arr[1];
+  else if (arr[2] === arr[5] && arr[2] === arr[8]) result = arr[2];
+  else if (arr[0] === arr[4] && arr[0] === arr[8]) result = arr[0];
+  else result = null;
+
+  return result;
+};
+
+module.exports = {
+  sumDigits,
+  createRange,
+  getScreentimeAlertList,
+  hexToRGB,
+  findWinner,
+};
---
+++ test/week10.test.js
@@ -0,0 +1,131 @@
+const {
+  sumDigits,
+  createRange,
+  getScreentimeAlertList,
+  hexToRGB,
+} = require("../challenges/week10");
+
+describe("sumDigits", () => {
+  test("n is not a number", () => {
+    expect(() => {
+      sumDigits("Hola");
+    }).toThrow(Error);
+    expect(() => {
+      sumDigits([1, 2, 3]);
+    }).toThrow(Error);
+    expect(() => {
+      sumDigits();
+    }).toThrow(Error);
+  });
+  test("given a number it returns the sum of all its digits", () => {
+    expect(sumDigits(123)).toBe(6);
+    expect(sumDigits(2891)).toBe(20);
+  });
+});
+
+describe("createRange", () => {
+  test("given a start, an end and step numbers create a range of numbers as an array", () => {
+    expect(createRange(3, 11, 2)).toEqual([3, 5, 7, 9, 11]);
+  });
+  test("Both the start and the end numbers are inclusive.", () => {
+    expect(createRange(7, 18, 2)).toEqual([7, 9, 11, 13, 15, 17, 18]);
+  });
+  test("Step is an optional parameter. If it is not provided, assume the step is 1.", () => {
+    expect(createRange(7, 10)).toEqual([7, 8, 9, 10]);
+  });
+  test("Step is a negative number, assume the step is 1.", () => {
+    expect(createRange(7, 10, -8)).toEqual([7, 8, 9, 10]);
+  });
+  test("If start number is greater than end number, the values ​​will be swapped.Start takes the number of End and vice versa.", () => {
+    expect(createRange(30, 10, 5)).toEqual([10, 15, 20, 25, 30]);
+  });
+});
+
+describe("getScreentimeAlertList", () => {
+  test("given an array of user objects we need to return an array of usernames who have used more than 100 minutes of screentime for a given date ", () => {
+    const users = [
+      {
+        username: "beth_1234",
+        name: "Beth Smith",
+        screenTime: [
+          {
+            date: "2019-05-01",
+            usage: { twitter: 34, instagram: 22, facebook: 40 },
+          },
+          {
+            date: "2019-05-02",
+            usage: { twitter: 56, instagram: 40, facebook: 31 },
+          },
+          {
+            date: "2019-05-03",
+            usage: { twitter: 12, instagram: 15, facebook: 19 },
+          },
+          {
+            date: "2019-05-04",
+            usage: { twitter: 10, instagram: 56, facebook: 61 },
+          },
+        ],
+      },
+      {
+        username: "sam_j_1989",
+        name: "Sam Jones",
+        screenTime: [
+          {
+            date: "2019-06-04",
+            usage: { mapMyRun: 40, whatsApp: 61, facebook: 0, safari: 10 },
+          },
+          {
+            date: "2019-06-13",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 },
+          },
+          {
+            date: "2019-06-14",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+          },
+        ],
+      },
+      {
+        username: "sam_t_2000",
+        name: "Sam Jones",
+        screenTime: [
+          {
+            date: "2019-05-04",
+            usage: { mapMyRun: 40, whatsApp: 61, facebook: 0, safari: 10 },
+          },
+          {
+            date: "2019-06-13",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 },
+          },
+          {
+            date: "2019-06-14",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+          },
+        ],
+      },
+    ];
+    expect(getScreentimeAlertList(users, "2019-05-04")).toEqual([
+      "beth_1234",
+      "sam_t_2000",
+    ]);
+  });
+});
+describe.only("hexToRGB", () => {
+  test("Given a string in hexadecimal format, this function must calculate and transform into a string in RGB format", () => {
+    expect(hexToRGB("#FF1133")).toBe("rgb(255,17,51)");
+    expect(hexToRGB("#000000")).toBe("rgb(0,0,0)");
+    expect(hexToRGB("#00FF00")).toBe("rgb(0,255,0)");
+  });
+  test("The function must not be case sensitive", () => {
+    expect(hexToRGB("#0000Ff")).toBe("rgb(0,0,255)");
+  });
+  test("If the hexadecimal letters are diferents to 'A,B,C,D,E,F' SHOW THE ERROR MESSAGE", () => {
+    expect(hexToRGB("#AU00FR")).toBe(
+      "This error is becouse you entered an incorrect character or the number of characters are incorrect, only 'A','B','C','D','E','F' are alowed"
+    );
+  });
+  test("If the numbers of characters are less than seven SHOW THE ERROR MESSAGE", () => {
+    expect(hexToRGB("#04FF0")).toBe(
+      "This error is becouse you entered an incorrect character or the number of characters are incorrect, only 'A','B','C','D','E','F' are alowed"
+    );
+  });
+});
